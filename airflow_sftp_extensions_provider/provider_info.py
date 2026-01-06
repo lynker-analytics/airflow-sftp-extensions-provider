06a7edc0de2c2bafa908b36166e0b98896591aa2
Achim Gaedke <achim.gaedke@lynker-analytics.com>
2026-01-07 10:09:14 +1300

    fixup airflow provider info

--- airflow_sftp_extensions_provider/provider_info.py
+++ airflow_sftp_extensions_provider/provider_info.py
@@ -1,10 +1,21 @@
-
 def get_provider_info():
     return {
         "package-name": "airflow-sftp-extensions-provider",
         "name": "sftp_extensions",
         "description": "SFTP server extension operators",
-        "operators": ["airflow_sftp_extensions_provider.StatvfsOperator"],
-        "hooks": ["airflow_sftp_extensions_provider.SFTPHookWExtension.SFTPHookWExtension"]
+        "operators": [
+            {
+                "python-modules": [
+                    "airflow_sftp_extensions_provider.SFTPExtensionOperators"
+                ]
+            }
+        ],
+        "hooks": [
+            {
+                "python-modules": [
+                    "airflow_sftp_extensions_provider.SFTPHookWExtension"
+                ],
+            }
+        ],
         # todo: add integrations?
     }
